e63c68616e77d331c16b474e2275c672f4bb7438
rfhtorres31 <torresronnierfranz@gmail.com>
2025-10-04 16:03:35 +0800

    Change RenjiDB to IRSDB for DBContextCS

--- IncidentUtility/Data/RenjiDbContext.cs
+++ IncidentUtility/Data/RenjiDbContext.cs
@@ -30,7 +30,7 @@ public partial class RenjiDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-P3T6S9M\\SQLEXPRESS;Database=RenjiDB;Trusted_Connection=True;Encrypt=False;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-P3T6S9M\\SQLEXPRESS;Database=IRSDB;Trusted_Connection=True;Encrypt=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
